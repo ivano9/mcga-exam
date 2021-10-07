ed0fdae43e6c88b1bd0b4b902cb55fe7ba0cd68f
Ivan Giovanazzi <ivanog@pm.me>
2021-10-07 15:11:48 -0300

    added-prettier-eslint-husky-configs

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "commonjs": true,
+    "es6": true
+  },
+  "parserOptions": {
+    "ecmaVersion": 12
+  },
+  "rules": {
+    "quotes": ["error", "single"],
+    "no-alert": ["error"],
+    "no-else-return": ["error"],
+    "arrow-body-style": ["error", "as-needed"]
+  },
+  "globals": {
+    "__dirname": "readonly",
+    "process": "readonly"
+  }
+}
