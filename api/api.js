902fd380651abb39735a586ff767d58b9eed3af7
Ivan Giovanazzi <ivanog@pm.me>
2021-10-07 19:18:04 -0300

    added-response-servidor-ok

--- api/api.js
+++ api/api.js
@@ -4,7 +4,7 @@ const express = require('express')
 
 const app = express()
 
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 
 app.use(express.json())
 
