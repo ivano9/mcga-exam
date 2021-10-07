9fe092e397ee8ec0aeccf86394993b510b9d0f31
Ivan Giovanazzi <ivanog@pm.me>
2021-10-07 19:17:40 -0300

    added-response-servidor-ok

--- api/api.js
+++ api/api.js
@@ -4,6 +4,13 @@ const express = require('express')
 
 const app = express()
 
+app.use(express.urlencoded({extended: true}))
+
+app.use(express.json())
+
+app.get('/', (_req, res) => res.send('Servidor ok'))
+
+app.use('/api', require('./routes'))
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
---
+++ api/routes/index.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const epxress = require('express')
+const router = epxress.Router()
+const fs = require('fs')
+
+const pathRouter = `${__dirname}`
+
+const removeExtension = (fileName) => fileName.split('.').shift()
+
+fs.readdirSync(pathRouter).filter((file) => {
+  const fileWithOutExt = removeExtension(file)
+  const skip = ['index'].includes(fileWithOutExt)
+  if (!skip) {
+    router.use(`/${fileWithOutExt}`, require(`./${fileWithOutExt}`))
+  }
+})
+
+router.get('*', (req, res) => {
+  res.status(404)
+  res.send({ error: 'Not found' })
+})
+
+module.exports = router
