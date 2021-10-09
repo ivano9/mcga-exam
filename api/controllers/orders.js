6dd280ec12b0488ead762edf72c2a8f5f8fb5ef3
Ivan Giovanazzi <ivanog@pm.me>
2021-10-09 15:25:32 -0300

    added-update-order

--- api/controllers/orders.js
+++ api/controllers/orders.js
@@ -38,8 +38,22 @@ const createOrder = (req, res) => {
   }
 }
 
+const updateOrder = (req, res) => {
+  const { id } = req.params
+  const data = req.body
+  try {
+    return ordersService.update(res, id, data)
+  } catch (err) {
+    return res.status(500).json({
+      message: err,
+      error: true,
+    })
+  }
+}
+
 module.exports = {
   getOrders,
   getOrderById,
   createOrder,
+  updateOrder,
 }
