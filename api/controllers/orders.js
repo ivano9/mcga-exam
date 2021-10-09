887aeafe494baa78d11a6635eb5dc7e29a81b57c
Ivan Giovanazzi <ivanog@pm.me>
2021-10-09 16:54:52 -0300

    fix-catch-error

--- api/controllers/orders.js
+++ api/controllers/orders.js
@@ -4,14 +4,7 @@ const { ordersService } = require('../service')
 
 const getOrders = (req, res) => {
   const query = req.query
-  try {
-    return ordersService.list(res, query)
-  } catch (err) {
-    return res.status(500).json({
-      data: `Something wrong while listing the customers. Error: ${err}`,
-      error: true,
-    })
-  }
+  return ordersService.list(res, query)
 }
 
 const getOrderById = (req, res) => {
