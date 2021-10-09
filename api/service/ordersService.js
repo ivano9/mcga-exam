b6fb9ec144b42a2f13594f2d9b0ff22f529bb36c
Ivan Giovanazzi <ivanog@pm.me>
2021-10-09 00:39:00 -0300

    fix-get-orders

--- api/service/ordersService.js
+++ api/service/ordersService.js
@@ -2,14 +2,14 @@
 
 const { ordersModel } = require('../models')
 
-const list = async (res, category) => {
-  if (!category)
+const list = async (res, customer_name) => {
+  if (!customer_name)
     return res.status(200).json({
       data: await ordersModel.find(),
       error: false,
     })
 
-  const orders = await ordersModel.find({ category }).exec()
+  const orders = await ordersModel.find({ customerName: customer_name }).exec()
   return res.status(200).json({
     data: !orders ? 'Not orders to show' : orders,
     error: false,
