90d42205513d423fd2857793269ca93559daebfb
Ivan Giovanazzi <ivanog@pm.me>
2021-10-09 00:57:46 -0300

    fix-query-string

--- api/service/ordersService.js
+++ api/service/ordersService.js
@@ -2,14 +2,14 @@
 
 const { ordersModel } = require('../models')
 
-const list = async (res, customer_name) => {
-  if (!customer_name)
+const list = async (res, query) => {
+  if (!query)
     return res.status(200).json({
       data: await ordersModel.find(),
       error: false,
     })
 
-  const orders = await ordersModel.find({ customerName: customer_name }).exec()
+  const orders = await ordersModel.find(query).exec()
   return res.status(200).json({
     data: !orders ? 'Not orders to show' : orders,
     error: false,
