6299cd61595ecd85896ecb0a99b732e8ebbb32b2
Ivan Giovanazzi <ivanog@pm.me>
2021-11-22 15:38:22 -0300

    added-cors

--- api/api.js
+++ api/api.js
@@ -2,12 +2,14 @@
 
 const express = require('express')
 const { connect } = require('./config/dbConnection')
+const cors = require('cors')
 
 const app = express()
 
 app.use(express.urlencoded({ extended: true }))
 
 app.use(express.json())
+app.use(cors())
 
 app.get('/', (_req, res) => res.send('Servidor ok'))
 
